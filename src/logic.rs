98c89ccc710d0cfe9a132a1139690577cb423d97
todesking <discommunicative@gmail.com>
2023-09-24 02:36:29 +0900

    Looks good

--- src/logic.rs
+++ src/logic.rs
@@ -2,34 +2,57 @@ use std::borrow::Cow;
 
 pub fn char_at(index: i64) -> u8 {
     assert!(index >= 0);
-    return b'X';
+    let index = index as u128;
+
+    let mut l = 0;
+    let mut r = i64::MAX;
+
+    while l + 1 < r {
+        let mid = l + (r - l) / 2;
+        let v = fizz_buzz_length(mid);
+        if v <= index {
+            l = mid;
+        } else {
+            r = mid;
+        }
+    }
+
+    let base = fizz_buzz_length(r - 1);
+    let line_index = (index - base) as usize;
+    let line = line_at(r);
+
+    line[line_index]
 }
 
 pub fn line_at(n: i64) -> Cow<'static, [u8]> {
     if n % 15 == 0 {
-        Cow::from(b"FizzBuzz" as &'static [u8])
+        Cow::from(b"FizzBuzz\n" as &'static [u8])
     } else if n % 3 == 0 {
-        Cow::from(b"Fizz" as &'static [u8])
+        Cow::from(b"Fizz\n" as &'static [u8])
     } else if n % 5 == 0 {
-        Cow::from(b"Buzz" as &'static [u8])
+        Cow::from(b"Buzz\n" as &'static [u8])
     } else {
-        let s = format!("{}", n);
+        let s = format!("{}\n", n);
         let v = Vec::from(s.as_bytes());
         Cow::from(v)
     }
 }
 
-pub fn fizz_buzz_length(n: i64) -> i64 {
+const NEWLINE_LEN: u128 = 1;
+
+pub fn fizz_buzz_length(n: i64) -> u128 {
     assert!(n >= 0);
-    let mut num_len_sum = 0i64;
-    let mut num_count = 0i64;
-    let mut fizz_count = 0i64;
-    let mut buzz_count = 0i64;
-    let mut fizz_buzz_count = 0i64;
-    let max = i64::MAX / 10 + 1;
-    let mut i = 10i64;
+    let n = n as u128;
+
+    let zero = 0u128;
+    let mut num_len_sum = zero;
+    let mut num_count = zero;
+    let mut fizz_count = zero;
+    let mut buzz_count = zero;
+    let mut fizz_buzz_count = zero;
+    let max = (i64::MAX / 10 + 1) as u128;
+    let mut i = 10u128;
     let mut digits = 1;
-    println!("Start: {}", n);
     while i <= n {
         let fizz_buzz_count0 = (i - 1) / 15 - fizz_buzz_count;
         let fizz_count0 = (i - 1) / 3 - fizz_buzz_count0 - fizz_count - fizz_buzz_count;
@@ -40,7 +63,7 @@ pub fn fizz_buzz_length(n: i64) -> i64 {
 
         let num_count0 = i - fizz_count - buzz_count - fizz_buzz_count - num_count;
         num_count += num_count0;
-        num_len_sum += (digits + 2) * num_count0;
+        num_len_sum += (digits + NEWLINE_LEN) * num_count0;
 
         assert_eq!(num_count + fizz_count + buzz_count + fizz_buzz_count, i);
 
@@ -61,12 +84,16 @@ pub fn fizz_buzz_length(n: i64) -> i64 {
     let num_count0 = n + 1 - fizz_count - buzz_count - fizz_buzz_count - num_count;
     num_count += num_count0;
 
-    num_len_sum += (digits + 2) * num_count0;
+    num_len_sum += (digits + NEWLINE_LEN) * num_count0;
 
     assert_eq!(fizz_count + buzz_count + fizz_buzz_count + num_count, n + 1);
 
-    // -3 for "0\r\n"
-    num_len_sum + fizz_count * 6 + buzz_count * 6 + fizz_buzz_count * 10 - 3
+    // 1 + NEWLINE_LEN for "0\n"
+    num_len_sum
+        + fizz_count * (4 + NEWLINE_LEN)
+        + buzz_count * (4 + NEWLINE_LEN)
+        + fizz_buzz_count * (8 + NEWLINE_LEN)
+        - (1 + NEWLINE_LEN)
 }
 
 #[cfg(test)]
@@ -74,37 +101,37 @@ mod test {
     use super::*;
 
     #[test]
-    fn test() {
+    fn test_length() {
         assert_eq!(fizz_buzz_length(0), 0);
-        assert_eq!(fizz_buzz_length(1), 3);
-        assert_eq!(fizz_buzz_length(2), 6);
-        assert_eq!(fizz_buzz_length(3), 12); // 3
-        assert_eq!(fizz_buzz_length(4), 15);
-        assert_eq!(fizz_buzz_length(5), 21); // 5
-        assert_eq!(fizz_buzz_length(6), 27); // 3
-        assert_eq!(fizz_buzz_length(7), 30);
-        assert_eq!(fizz_buzz_length(8), 33);
-        assert_eq!(fizz_buzz_length(9), 39); // 3
-        assert_eq!(fizz_buzz_length(10), 45); // 5
-        assert_eq!(fizz_buzz_length(11), 49);
-        assert_eq!(fizz_buzz_length(12), 55); // 3
-        assert_eq!(fizz_buzz_length(13), 59);
-        assert_eq!(fizz_buzz_length(14), 63);
-        assert_eq!(fizz_buzz_length(15), 73); // 35
-        assert_eq!(fizz_buzz_length(16), 77);
+        assert_eq!(fizz_buzz_length(1), 2);
+        assert_eq!(fizz_buzz_length(2), 4);
+        assert_eq!(fizz_buzz_length(3), 9); // 3
+        assert_eq!(fizz_buzz_length(4), 11);
+        assert_eq!(fizz_buzz_length(5), 16); // 5
+        assert_eq!(fizz_buzz_length(6), 21); // 3
+        assert_eq!(fizz_buzz_length(7), 23);
+        assert_eq!(fizz_buzz_length(8), 25);
+        assert_eq!(fizz_buzz_length(9), 30); // 3
+        assert_eq!(fizz_buzz_length(10), 35); // 5
+        assert_eq!(fizz_buzz_length(11), 38);
+        assert_eq!(fizz_buzz_length(12), 43); // 3
+        assert_eq!(fizz_buzz_length(13), 46);
+        assert_eq!(fizz_buzz_length(14), 49);
+        assert_eq!(fizz_buzz_length(15), 58); // 35
+        assert_eq!(fizz_buzz_length(16), 61);
     }
     #[test]
-    fn test_many() {
-        let mut l = 0i64;
+    fn test_length_many() {
+        let mut l = 0u128;
         for i in 1..=11111 {
             if i % 3 == 0 && i % 5 == 0 {
-                l += 10;
+                l += 8 + NEWLINE_LEN;
             } else if i % 3 == 0 {
-                l += 6;
+                l += 4 + NEWLINE_LEN;
             } else if i % 5 == 0 {
-                l += 6;
+                l += 4 + NEWLINE_LEN;
             } else {
-                l += i.to_string().len() as i64 + 2;
+                l += i.to_string().len() as u128 + NEWLINE_LEN;
             }
             let actual = fizz_buzz_length(i as i64);
             assert_eq!(
@@ -113,5 +140,32 @@ mod test {
                 i, actual, l
             );
         }
+    }
+    #[test]
+    fn test_char_at() {
+        assert_eq!(char_at(0), b'1');
+        assert_eq!(char_at(1), b'\n');
+
+        let expected = b"1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16";
+        let mut actual = Vec::new();
+        for i in 0..expected.len() {
+            actual.push(char_at(i as i64));
+        }
+        assert_eq!(expected, actual.as_slice());
     }
 }
